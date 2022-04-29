be592846c2264ff7b7b99989298bd60ea0c1dd10
akza <itsakza07@gmail.com>
2022-04-29 21:11:22 +0530

    Added settings menu

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 console.log("It's working");
 // Watches the clicks on Date
-document.querySelector(".date").addEventListener("click", () => showNotifications())
+document.querySelector(".date").addEventListener("click", () => showNotifications(".date",".notif"))
+
+// Watches the clicks on menu
+document.querySelector(".menu").addEventListener("click", () => showNotifications(".menu",".menu-expanded"))
 
 
 // Opens notification panel
-function showNotifications() {
-    panelVisible = document.querySelector(".notif-panel").style;    
-    if (panelVisible.opacity === "0") {
-        panelVisible.opacity = "1"
-        document.querySelector('.date').classList.add('active')
+function showNotifications(clickable,menuName) {
+    panel = document.querySelector(menuName).style;
+    if (panel.opacity === "0") {
+        panel.opacity = "1"
+        document.querySelector(clickable).classList.add('active')
     } else {
-        panelVisible.opacity = "0"
-        document.querySelector('.date').classList.remove('active')
-
+        panel.opacity = "0"
+        document.querySelector(clickable).classList.remove('active')
     }
 }
 
