647a78180990fe1a218c2e5be8d1924943503e28
akzaisme <itsakza07@gmail.com>
2025-12-10 23:44:00 +0530

    Handle tailwind darkmode classes for lightmod

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,38 +9,41 @@
 
   function closeModal() {
     localStorage.setItem('tos_accepted', 'yes');
+    modalElement.classList.add('hidden');
     modalElement.close();
   }
 </script>
 
 <dialog
-  class="m-auto! flex items-center justify-center rounded bg-gray-700 outline-none dark:text-white"
+  class="m-auto! flex items-center justify-center rounded outline-none dark:bg-gray-700 dark:text-white"
   bind:this={modalElement}
 >
-  <div class="prose p-5 prose-invert">
+  <div class="prose p-5 dark:prose-invert">
     <div class="text-center text-2xl font-bold">Note</div>
     <ol>
       <li>This page is intentionally monospace.</li>
       <li>I do not collect any of your personal data from the browser.</li>
     </ol>
     <button
-      class="border-white-1 mx-auto mt-3 block rounded border bg-gray-800 px-3 outline-white"
+      class="border-white-1 mx-auto mt-3 block rounded border px-3 dark:bg-gray-800 dark:outline-white"
       onclick={closeModal}>Close</button
     >
   </div>
 </dialog>
 <div class="relative min-h-screen w-auto">
-  <div class="flex items-center justify-center bg-slate-800 p-10 font-mono text-2xl tracking-wide">
-    <div class=" prose min-w-3/5 dark:prose-invert">
+  <div
+    class="flex items-center justify-center p-10 font-mono text-2xl tracking-wide dark:bg-slate-800"
+  >
+    <div class="prose min-w-3/5 dark:prose-invert">
       <div class="space-y-4 text-center">
-        <h1 class="font-mono text-6xl font-bold tracking-widest text-slate-100">akza07</h1>
-        <p class="mx-auto max-w-xl text-xl text-slate-300">
+        <h1 class="font-mono text-6xl font-bold tracking-widest dark:text-slate-100">akza07</h1>
+        <p class="mx-auto max-w-xl text-xl dark:text-slate-300">
           Backend-focused engineer who builds fast APIs, distributed workers, and queue-driven
           systems.
         </p>
       </div>
       <hr />
-      <h2 class="text-slate-300 underline underline-offset-8">Background</h2>
+      <h2 class="underline underline-offset-8 dark:text-slate-300">Background</h2>
       <ul>
         <li><strong>Education: </strong> B-Tech Computer Science.</li>
         <li><strong>Work Experience: </strong> 4+ years as Software Engineer.</li>
@@ -49,8 +52,8 @@
       </ul>
       <hr />
 
-      <h2 class="text-slate-300 underline underline-offset-8">Languages & Stacks</h2>
-      <ul class="text-slate-400">
+      <h2 class="underline underline-offset-8 dark:text-slate-300">Languages & Stacks</h2>
+      <ul class="dark:text-slate-400">
         <li><strong>Languages:</strong> Node.js, Python, Go</li>
         <li><strong>Backend Frameworks:</strong> NestJS, Django REST Framework</li>
         <li><strong>Frontend Bits:</strong> Svelte, React, HTML, CSS</li>
@@ -61,8 +64,10 @@
       </ul>
       <hr />
 
-      <h2 class="text-slate-300 underline underline-offset-8">3rd Party Integrations & Services</h2>
-      <ul class="text-slate-400">
+      <h2 class="underline underline-offset-8 dark:text-slate-300">
+        3rd Party Integrations & Services
+      </h2>
+      <ul class="dark:text-slate-400">
         <li>
           <strong>iOS Subscriptions</strong> — App Store Server Notifications (V2), receipt validation.
         </li>
@@ -85,14 +90,14 @@
         </li>
       </ul>
 
-      <p class="text-slate-400">
+      <p class="dark:text-slate-400">
         If it involves webhooks, signatures, async events, or weird third-party quirks, I’ve
         probably debugged it.
       </p>
       <hr />
 
-      <h2 class="text-slate-300 underline underline-offset-8">Hands-on Experiences</h2>
-      <ul class="text-slate-400">
+      <h2 class="underline underline-offset-8 dark:text-slate-300">Hands-on Experiences</h2>
+      <ul class="dark:text-slate-400">
         <li><strong>Authentication</strong> with JWT with token rotation, OAuth2 etc.</li>
         <li><strong>Microservices</strong> for high-volume event processing with RabbitMQ</li>
         <li><strong>Subscription and Payment automation</strong> pipelines</li>
@@ -105,13 +110,13 @@
         <li>
           <strong>Webhook-driven Systems</strong> with retries, DLQs, and signature verification
         </li>
-        <p class="text-slate-400">
+        <p class="dark:text-slate-400">
           APIs, real-time services, distributed workers, internal tools, automation scripts, and
           systems designed to so that they remain easier to maintain and scalable when needed.
         </p>
       </ul>
 
-      <p class="text-slate-400">
+      <p class="dark:text-slate-400">
         If it runs in the background or handles load quietly without fuzz, I probably enjoyed
         building it.
       </p>
