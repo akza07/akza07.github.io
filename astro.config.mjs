5950174d126ed2e9bae2c5d2a39cf794bda0b119
akza07 <itsakza07@gmail.com>
2026-06-11 23:23:24 +0530

    Minor repository updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://akza07.github.io',
-  base: '/akza07.github.io/',
+  base: '/',
 });
